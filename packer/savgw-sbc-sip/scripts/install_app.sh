05ed04fec18f15f2704c684b7a6ae3206becc024
nitinpo-Kore <nitin.poojary@kore.com>
2023-04-04 18:54:02 +0530

    folder File name change

--- packer/savgw-sbc-sip/scripts/install_app.sh
+++ packer/savgw-sbc-sip/scripts/install_app.sh
@@ -11,7 +11,7 @@ cd /home/admin/apps/sbc-outbound && sudo npm install --unsafe-perm
 cd /home/admin/apps/sbc-registrar && sudo npm install --unsafe-perm
 cd /home/admin/apps/sbc-call-router && sudo npm install --unsafe-perm
 cd /home/admin/apps/sbc-options-handler && sudo npm install --unsafe-perm
-cd /home/admin/apps/jambonz-smpp-esme && sudo npm install --unsafe-perm
+cd /home/admin/apps/savgw-smpp-esme && sudo npm install --unsafe-perm
 
 sudo -u admin bash -c "pm2 install pm2-logrotate"
 sudo -u admin bash -c "pm2 set pm2-logrotate:max_size 1G"
