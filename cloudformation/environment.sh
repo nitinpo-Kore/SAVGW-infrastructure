c26290ef3d505fedaea5641810f02d5718672adb
nitinpo-Kore <nitin.poojary@kore.com>
2023-04-09 13:43:42 +0530

    cloudformation environment added

--- cloudformation/environment.sh
+++ cloudformation/environment.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-export JAMBONES_MYSQL_HOST="10.160.0.15"
-export JAMBONES_MYSQL_USER="admin"
-export JAMBONES_MYSQL_PASSWORD="Kore#123"
+export JAMBONES_MYSQL_HOST="[Enter MYSQL HOSTNAME or Local IP]"
+export JAMBONES_MYSQL_USER="[Enter MYSQL Username]"
+export JAMBONES_MYSQL_PASSWORD="[Enter MYSQL Password]"
 export DbCachingTTS="30"
 
-export JAMBONES_REDIS_HOST="10.160.0.15"
-export JAMBONES_CLUSTER_ID="savg"
-export JAMBONES_TIME_SERIES_HOST="10.160.0.8"
+export JAMBONES_REDIS_HOST="[Enter REDIS HOSTNAME or Local IP]"
+export JAMBONES_CLUSTER_ID="savgw"
+export JAMBONES_TIME_SERIES_HOST="[Enter MONITORING SERVER Local IP]"
 export JAMBONES_SAMPLE_RATE="1"
-export JAMBONES_SBC_SIP_IPS="sbc1-savg.federalbank.co.in"
-export JAMBONES_RTPENGINE_IPS="34.100.143.35"
+export JAMBONES_SBC_SIP_IPS="[Enter Domain Name of SIP Server for Example sbc1-savg.domainname.co.in]"
+export JAMBONES_RTPENGINE_IPS="[Enter RTP Engine Public IP]"
 
 
-export MONITORING_SERVER_IP="10.160.0.8"
-export URLPortal="savg.federalbank.co.in"
+export MONITORING_SERVER_IP="[Enter MONITORING SERVER Local IP]"
+export URLPortal="[savg.domainname.co.in]"
 export JaegerUsername="Kore"
 export JaegerPassword="Kore@123"
 export CloudwatchLogRetention="10"
-export VPC_CIDR="10.160.0.0/24"
+export VPC_CIDR="[Enter VPC Subnet for example :10.160.0.0/24]"
 export JWT_SECRET="8q%Ed2f3H4*#v2Bn"
 export OTEL_TRACING="1"
 
-export AWS_REGION="US"
+export AWS_REGION="[Enter The AWS Reagion Name]"
 export AWS_SNS_TOPIC_ARN
-export AWS_REGION
+
 
 
 
